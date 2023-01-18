96f0c12169433c0e254f18aab9a8801aa89c23d0
olsi10 <wangbampang2035@gmail.com>
2023-01-18 20:32:39 +0900

    흑흑

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
 import Header from './components/Header';
 import Member from './components/Member';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Project from './components/Project';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header type = "2"></Header>
       <Member></Member>
+
+      <BrowserRouter>
+            {/* <Header/> */}
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/member" element={<Member/>}/>
+                <Route path="/project" element={<Project/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
