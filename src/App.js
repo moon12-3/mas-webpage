cbd733c29613af9df79b956c144aeb5dffadc458
olsi10 <wangbampang2035@gmail.com>
2023-01-27 03:53:17 +0900

    개인 프로젝트 페이지

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import Header from './components/Header';
 import Member from './components/Member';
 import Project from './components/Project';
 import Individual from './components/Individual';
+import IndividualProject from './components/IndividualProject';
 
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes, Link, useNavigate} from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
-  
 
   return (
     <BrowserRouter>
-      <Header type = "2"></Header>
+      <Header type="2"></Header>
         <Routes>
-          <Route path="/" element={<Main/>}/>
-          <Route path="/member" element={<Member/>}/>
-          <Route path="/indi/:name" element={<Individual/>}/>
-          <Route path="/project" element={<Project/>}/>
+          <Route path="/" element={<Main />} />
+          <Route path="/member" element={<Member />} />
+          <Route path="/indi/:name" element={<Individual />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="IndividualProject" element={<IndividualProject />} />
         </Routes>
     </BrowserRouter>
   );
