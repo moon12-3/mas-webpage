b575f85cd7d754cd2851482b1492967c6a442a19
olsi10 <wangbampang2035@gmail.com>
2023-01-27 03:54:19 +0900

    프로젝트 페이지 Link

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,7 @@
 import styles from './Member.module.css';
 import React from 'react';
+import dummy from "../db/data.json";
+import { Link } from 'react-router-dom';
 // import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function Project() {
@@ -15,13 +17,19 @@ function Project() {
 }
 
 function Footer2() {
+
+    const [setGrade] = React.useState("1");
+    // let list = dummy.people.filter(people => people.id === grade);
+
     return(
         <div className={styles.footer1}>
             <div className={styles.section}>
-                <div className={styles.fImg}></div>
-                <div className={styles.ftitle2}>프로젝트명</div>
-                <a href="#!" className={styles.fcontent1}>2022.07.25~2022.07.30</a><br/>
-                <a href="#!" className={styles.fcontent2}>소개를 쓰십시오</a>
+                <Link to={'/individualproject'}>
+                    <div className={styles.fImg}></div>
+                    <div className={styles.ftitle2}>프로젝트명</div>
+                    <a href="#!" className={styles.fcontent1}>2022.07.25~2022.07.30</a><br/>
+                    <a href="#!" className={styles.fcontent2}>소개를 쓰십시오</a>
+                </Link>
             </div>
             <div className={styles.section}>
                 <div className={styles.fImg}></div>
@@ -35,7 +43,26 @@ function Footer2() {
                 <a href="#!" className={styles.fcontent1}>2022.07.25~2022.07.30</a><br/>
                 <a href="#!" className={styles.fcontent2}>소개를 쓰십시오</a>
             </div>       
+
+            <div className="grade">
+                    <ul className="grade-list">
+                        {
+                        dummy.grades.map(grade => (
+                            <li key={grade.id}>
+                                <button onClick={()=>{setGrade(grade.id)}}>
+                                <p>
+                                    2021 {grade.grade}
+                                </p>
+                                </button>
+                            </li>
+                        ))
+ 
+                        }
+                    </ul>
+            </div>
+            
         </div>
+        
     )
 }
 
